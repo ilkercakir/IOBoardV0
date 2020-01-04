46dc1b74c2a61940fe626ee19afd10140048bd3f
Ilker Ismail Cakir <ilker.cakir13@gmail.com>
2020-01-04 16:03:10 +0300

    Update devices_insert.sql

--- DBScripts/devices_insert.sql
+++ DBScripts/devices_insert.sql
@@ -1,6 +1,8 @@
-insert into devices values(0, 'Pool Motor', 1, 0, 0, 'motor.png');
-insert into devices values(1, 'Pool Lamp', 1, 0, 0, 'lamp.png');
-insert into devices values(8, 'Enable', 1, 0, 0, 'enable.png');
-insert into devices values(9, 'Led', 1, 0, 0, 'led.png');
-insert into devices values(10, 'Bell', 3, 0, 0, 'bell.png');
-insert into devices values(11, 'Door Automat', 3, 0, 0, 'automat.png');
+insert into devices values(0, 'Pool Motor', 1, 0, 0, 0, 'motor.png');
+insert into devices values(1, 'Pool Lamp', 1, 1, 0, 0, 'lamp.png');
+insert into devices values(8, 'Enable', 1, 8, 0, 0, 'enable.png');
+insert into devices values(9, 'Led', 1, 9, 0, 0, 'led.png');
+insert into devices values(10, 'Bell', 3, 10, 0, 0, 'bell.png');
+insert into devices values(11, 'Door Automat', 3, 12, 0, 0, 'automat.png');
+insert into devices values(12, 'Sensor 1', 5, 0, 0, 0, 'bell.png');
+insert into devices values(13, 'Sensor 2', 6, 1, 8, 0, 'automat.png');
