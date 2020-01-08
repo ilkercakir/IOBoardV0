2236ae1fabc88dcacbbcd730220e5da02d943fe8
Ilker Ismail Cakir <ilker.cakir13@gmail.com>
2020-01-08 11:25:30 +0300

    Update rules.sql

--- DBScripts/rules.sql
+++ DBScripts/rules.sql
@@ -1,10 +1,8 @@
 create table rules
 (
-sgrid int NOT NULL,
-agrid int NOT NULL,
-interval NOT NULL,
-primary key (sgrid, agrid),
-foreign key (sgrid) references devicegroups(devgr),
-foreign key (sgrid) references devicegroups(devgr),
-foreign key (interval) references intervals(intid),
+rulid int NOT NULL,
+rultx char(30),
+intid int,
+primary key (rulid),
+foreign key intid references intervals(intid)
 );
