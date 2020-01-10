df8b17583fc26e7c16d71e0e96918d8ff391e642
Ilker Ismail Cakir <ilker.cakir13@gmail.com>
2020-01-10 09:01:10 +0300

    Update rules.h

--- C/rules.h
+++ C/rules.h
@@ -1,1 +1,36 @@
+#ifndef rulesH
+#define rulesH
 
+typedef struct
+{
+  int chnnl;
+  int devid;
+  int fromval;
+  int toval;
+}sdev;
+
+typedef struct
+{
+  int chnnl;
+  int devid;
+  int value;
+}adev;
+
+typedef struct
+{
+  int rulid;
+  int sgrid;
+  int agrid;
+  sdev *sdevs;
+  adev *adevs;
+}rule;
+
+typedef struct
+{
+  int intid;
+  char intxt[30];
+  int interval;
+  rule *rules;
+}interval;
+
+#endif
