035a8b64693c2ba449951b4d5e763808e5cf3af8
Ilker Ismail Cakir <ilker.cakir13@gmail.com>
2020-01-10 13:08:59 +0300

    Update rules.c

--- C/rules.c
+++ C/rules.c
@@ -1,8 +1,87 @@
 #include "rules.h"
 #include <sqlite3.h>
 
+int init_rules_of_interval_callback(void *data, int argc, char **argv, char **azColName) 
+{
+  interval *i = (interval *)data;
+  
+  i->rules[i->rulecount].rulid = atoi(argv[0]);
+  i->rules[i->rulecount].sgrid = atoi(argv[1]);
+  i->rules[i->rulecount].agrid = atoi(argv[2]);  
+  init_count_sensors_of_rule(&(s->intervals[s->intervalcount]));
+  init_sensors_of_rule(&(s->intervals[s->intervalcount]));
+
+  s->rulecount++;
+
+  return(0);
+}
+
 void init_rules_of_interval(interval *i)
 {
+  sqlite3 *db;
+  char *err_msg = NULL;
+  char sql[100];
+  int rc;
+
+  if ((rc = sqlite3_open(DBPATH, &db)))
+  {
+    printf("Can't open database: %s\n", sqlite3_errmsg(db));
+  }
+  else
+  {
+//printf("Opened database successfully\n");
+    sprintf(sql, "select ruledef.rulid, ruledef.sgrid, ruledef.agrid from rules inner join ruledef on rules.rulid = ruledef.rulid where rules.intid = %d;", i->intid);
+    if((rc = sqlite3_exec(db, sql, init_intervals_callback, (void*)i, &err_msg)) != SQLITE_OK)
+    {
+      printf("Failed to select data, %s\n", err_msg);
+      sqlite3_free(err_msg);
+    }
+    else // success
+    {
+      
+    }
+  }
+  sqlite3_close(db);
+}
+
+int init_count_rules_of_interval_callback(void *data, int argc, char **argv, char **azColName) 
+{
+  interval *i = (interval *)data;
+  
+  i->rulecount = atoi(argv[0]);
+  i->rules = malloc(i->rulecount*sizeof(rule));
+
+  i->rulecount = 0;
+
+  return(0);
+}
+
+void init_count_rules_of_interval(interval *i)
+{
+  sqlite3 *db;
+  char *err_msg = NULL;
+  char sql[100];
+  int rc;
+
+  if ((rc = sqlite3_open(DBPATH, &db)))
+  {
+    printf("Can't open database: %s\n", sqlite3_errmsg(db));
+  }
+  else
+  {
+//printf("Opened database successfully\n");
+    sprintf(sql, "select count(*) as rulecount from rules where intid = %d;", i->intid);
+    if((rc = sqlite3_exec(db, sql, init_intervals_callback, (void*)i, &err_msg)) != SQLITE_OK)
+    {
+      printf("Failed to select data, %s\n", err_msg);
+      sqlite3_free(err_msg);
+    }
+    else // success
+    {
+      
+    }
+  }
+  sqlite3_close(db);
 }
 
 int init_intervals_callback(void *data, int argc, char **argv, char **azColName) 
@@ -11,6 +90,7 @@ int init_intervals_callback(void *data, int argc, char **argv, char **azColName)
   
   s->intervals[s->intervalcount].intid = atoi(argv[0]);
   s->intervals[s->intervalcount].seconds = atoi(argv[2]);  
+  init_count_rules_of_interval(&(s->intervals[s->intervalcount]));
   init_rules_of_interval(&(s->intervals[s->intervalcount]));
 
   s->intervalcount++;
@@ -51,7 +131,7 @@ int init_scheduler_callback(void *data, int argc, char **argv, char **azColName)
   scheduler *s = (scheduler *)data;
   
   s->intervalcount = atoi(argv[0]);
-  s->intervals = malloc(s->count*sizeof(interval));
+  s->intervals = malloc(s->intervalcount*sizeof(interval));
 
   s->intervalcount = 0;
 
