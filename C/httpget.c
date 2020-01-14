2e1583fb945f47f5cf3f69ede3bc3ef70822752e
Ilker Ismail Cakir <ilker.cakir13@gmail.com>
2020-01-14 22:34:19 +0300

    Add files via upload

---
+++ C/httpget.c
@@ -0,0 +1,111 @@
+#include "httpget.h"
+
+void http_get_init(httpclient *h)
+{
+	h->portno = 8080;
+	h->host = "localhost";
+	h->getformat = "GET /IOBoardV0/ControllerConsole?%s%s HTTP/1.0\r\n\r\n";
+}
+
+void http_get_command(httpclient *h, char *command, char *parameters)
+{
+	sprintf(h->request, h->getformat, command, parameters);
+//printf("Request: %s\n", h->request);
+}
+
+void http_get(httpclient *h)
+{
+	struct hostent *server;
+	struct sockaddr_in serv_addr;
+	int sockfd, bytes, sent, received, total;
+
+	/* create the socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		printf("ERROR opening socket\n");
+		return;
+	}
+
+	/* lookup the ip address */
+	server = gethostbyname(h->host);
+	if (server == NULL)
+	{
+		printf("ERROR, no such host\n");
+		return;
+	}
+
+	/* fill in the structure */
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(h->portno);
+	memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+
+	/* connect the socket */
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("ERROR connecting\n");
+		return;
+	}
+
+	/* send the request */
+	total = strlen(h->request);
+	sent = 0;
+	do
+	{
+		bytes = write(sockfd, h->request+sent, total-sent);
+		if (bytes < 0)
+			printf("ERROR writing request to socket\n");
+        if (bytes == 0)
+            break;
+		sent+=bytes;
+	}while (sent < total);
+
+	/* receive the response */
+	memset(h->response, 0, sizeof(h->response));
+	total = sizeof(h->response) - 1;
+	received = 0;
+	do
+	{
+		bytes = read(sockfd, h->response+received, total-received);
+		if (bytes < 0)
+			printf("ERROR reading response from socket\n");
+		if (bytes == 0)
+			break;
+		received+=bytes;
+	}while(received < total);
+
+	if (received == total)
+	{
+		printf("ERROR storing complete response from socket\n");
+		return;
+	}
+
+	/* close the socket */
+	close(sockfd);
+
+	/* skip headers, convert response to JSON */
+	int pipes[2];
+	FILE *writepipe;
+	FILE *readpipe;
+	pipe(pipes);
+
+	readpipe = fdopen(pipes[0], "r");
+	writepipe = fdopen(pipes[1], "w");
+	fputs(h->response, writepipe);
+	fclose(writepipe);
+
+	while(fgets(h->json, sizeof(h->json), readpipe) != NULL)
+    {
+		if (!strcmp(h->json, "\r\n"))
+			break;
+	}
+/*
+	while (fgets(h->json, sizeof(h->json), readpipe) != NULL)
+	{
+		printf("--> %s\n", h->json);
+	}
+*/
+	fgets(h->json, sizeof(h->json), readpipe);
+	fclose(readpipe);
+}
---
+++ C/httpget.h
@@ -0,0 +1,25 @@
+#ifndef httpgetH
+#define httpgetH
+
+#include <stdio.h> /* printf, sprintf */
+#include <stdlib.h> /* exit */
+#include <unistd.h> /* read, write, close */
+#include <string.h> /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h> /* struct hostent, gethostbyname */
+
+
+typedef struct
+{
+	int portno;
+	char *host;
+	char *getformat;
+
+	char request[100], response[300], json[100];
+}httpclient;
+
+void http_get_init(httpclient *h);
+void http_get_command(httpclient *h, char *command, char *parameters);
+void http_get(httpclient *h);
+#endif
